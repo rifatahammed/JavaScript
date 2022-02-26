e028efb7c767cdf6c3fb04441d447bb70e6fbf25
rifatahammed <reardj007@gmail.com>
2022-02-27 02:19:33 +0600

    Sketch JS commented

--- Coding Challenge #1 Starfield in Processing/Sketch.js
+++ Coding Challenge #1 Starfield in Processing/Sketch.js
@@ -3,16 +3,16 @@ var stars = [];
 var speed;
 
 function setup() {
-  createCanvas(1600, 800);
+  createCanvas(1600, 800);//https://p5js.org/reference/#/p5/createCanvas
   for (var i = 0; i < 800; i++) {
     stars[i] = new Star();
   }
 }
 
 function draw() {
-  speed = map(mouseX, 0, width, 0, 50);
+  speed = map(mouseX, 0, width, 0, 50); //https://p5js.org/reference/#/p5/map
   background(0); // this means black bg
-  translate(width / 2, height / 2);
+  translate(width / 2, height / 2);//https://p5js.org/reference/#/p5/translate
   for (var i = 0; i < stars.length; i++) {
     stars[i].update();
     stars[i].show();
