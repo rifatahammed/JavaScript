7932c9738f12ea1421c7d6ab773c9b0fb4f40c7e
rifatahammed <reardj007@gmail.com>
2022-02-27 02:23:25 +0600

    Star.js comments added

--- Coding Challenge #1 Starfield in Processing/Star.js
+++ Coding Challenge #1 Starfield in Processing/Star.js
@@ -1,3 +1,8 @@
+/*In JavaScript, the this keyword refers to an object.
+Which object depends on how this is being invoked (used or called).
+The this keyword refers to different objects depending on how it is used:*/
+//https://www.w3schools.com/js/js_this.asp
+
 function Star() {
     this.x = random(-width, width);
     this.y = random(-height, height);
@@ -22,15 +27,15 @@ function Star() {
       var sy = map(this.y / this.z, 0, 1, 0, height);
   
       var r = map(this.z, 0, width, 16, 0);
-      ellipse(sx, sy, r, r);
+      ellipse(sx, sy, r, r);//https://p5js.org/reference/#/p5/ellipse
   
       var px = map(this.x / this.pz, 0, 1, 0, width);
       var py = map(this.y / this.pz, 0, 1, 0, height);
   
       this.pz = this.z;
   
-      stroke(255);
-      line(px, py, sx, sy);
+      stroke(255);//https://p5js.org/reference/#/p5/stroke
+      line(px, py, sx, sy);//https://p5js.org/reference/#/p5/line
   
     }
   }
